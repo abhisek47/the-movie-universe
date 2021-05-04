21b476f3e61abc63f6ac6bb4b022932028c6eeee
abhisek47 <abhisek529@gmail.com>
2021-05-04 17:25:43 +0530

    modify movie component

--- src/components/Search.js
+++ src/components/Search.js
@@ -44,11 +44,11 @@ const Search = () => {
               Search Movies
             </button>
           </form>
-          <div className='my-10 grid grid-cols-1 md:grid-cols-4 gap-4'>
+          <div className='my-10 grid grid-cols-2 md:grid-cols-4 gap-4'>
             {movies
               .filter((movie) => movie.poster_path)
               .map((movie) => (
-                <Movie movie={movie} />
+                <Movie movie={movie} key={movie.id} />
               ))}
           </div>
         </div>
