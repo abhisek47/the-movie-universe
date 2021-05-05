f96c9372cada2410a0361f516e0de8871157243d
abhisek47 <abhisek529@gmail.com>
2021-05-05 13:47:11 +0530

    add framer-motion

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import Search from './components/Search';
+import { motion } from 'framer-motion';
 
 const App = () => {
   return (
     <>
       <section className='bg-gray-800'>
         <div className='container mx-auto'>
-          <h1 className='text-2xl text-gray-50 text-center p-5'>
-            The Movie Universe
-          </h1>
+          <motion.div
+            initial='hidden'
+            animate='visible'
+            variants={{
+              hidden: {
+                scale: 0.8,
+                opacity: 0,
+              },
+              visible: {
+                scale: 1,
+                opacity: 1,
+                transition: {
+                  delay: 0.5,
+                },
+              },
+            }}>
+            <h1 className='text-2xl text-gray-50 text-center p-5'>
+              The Movie Universe
+            </h1>
+          </motion.div>
         </div>
       </section>
       <Search />
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,19 +1,46 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 
 const Movie = ({ movie }) => {
   return (
-    <div className='my-2 w-full bg-gray-50 p-5' key={movie.id}>
-      <div>
-        <img
-          src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
-          alt={movie.title + ' poster'}
-        />
+    <motion.div
+      initial='hidden'
+      animate='visible'
+      variants={{
+        hidden: {
+          scale: 0.8,
+          opacity: 0,
+        },
+        visible: {
+          scale: 1,
+          opacity: 1,
+        },
+      }}>
+      <div className='my-2 w-full bg-gray-50 p-5' key={movie.id}>
+        <motion.div
+          whileHover={{
+            position: 'relative',
+            zIndex: 1,
+            background: 'white',
+            scale: 1.1,
+            transition: {
+              duration: 0.2,
+            },
+          }}>
+          <div className='cursor-pointer'>
+            <img
+              className='w-full'
+              src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
+              alt={movie.title + ' poster'}
+            />
+          </div>
+        </motion.div>
+        <div className='pt-6'>
+          <h1 className='text-base md:text-lg'>{movie.title}</h1>
+          <p className='my-2 text-gray-500 text-sm'>{movie.release_date} </p>
+        </div>
       </div>
-      <div className='py-4'>
-        <h1 className='text-base md:text-lg'>{movie.title}</h1>
-        <p className='my-2 text-gray-500 text-sm'>{movie.release_date} </p>
-      </div>
-    </div>
+    </motion.div>
   );
 };
 
